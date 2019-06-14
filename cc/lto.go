b4a98615aa9151d3da0a9ab0eef8340afbe0749c
Pirama Arumuga Nainar <pirama@google.com>
2019-06-14 09:54:23 -0700

    Remove redundant -fexperimental-new-pass-manager
    This is now turned on by default (cc/config/clang.go).
    
    Test: N/A
    Change-Id: I61bcf2049448b4200ca303ebcad555cc24a43c18
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- cc/lto.go
+++ cc/lto.go
@@ -107,8 +107,9 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 		// If the module does not have a profile, be conservative and do not inline
 		// or unroll loops during LTO, in order to prevent significant size bloat.
 		if !ctx.isPgoCompile() {
-			flags.LdFlags = append(flags.LdFlags, "-Wl,-plugin-opt,-inline-threshold=0")
-			flags.LdFlags = append(flags.LdFlags, "-Wl,-plugin-opt,-unroll-threshold=0")
+			flags.LdFlags = append(flags.LdFlags,
+				"-Wl,-plugin-opt,-inline-threshold=0",
+				"-Wl,-plugin-opt,-unroll-threshold=0")
 		}
 	}
 	return flags
