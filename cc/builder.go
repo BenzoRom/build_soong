5049c7fb940f98021a6e2ebe85b67025e52a21fc
Dan Willemsen <dwillemsen@google.com>
2019-08-09 12:45:53 -0700

    Include dependencies for clang-tidy and header-abi-dumper
    We've been getting these dependencies transitively through the
    dependency on the object itself (which is a workaround for the lack of
    dep file support in these tools). But for remote builds to work, we need
    to know about these dependencies like any other object compilation.
    
    For regular builds, this increases the size of the ninja file by a few
    tens of megabytes (~1-2%).
    
    WITH_TIDY builds were already larger (~40-50%), but are now about 90%
    larger than a normal build.
    
    Test: treehugger
    Change-Id: Icdb4ca3d4d08d5706593d96d5c627149fa14fed8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- cc/builder.go
+++ cc/builder.go
@@ -479,7 +479,9 @@ func TransformSourceToObj(ctx android.ModuleContext, subdir string, srcFiles and
 				Input:       srcFile,
 				// We must depend on objFile, since clang-tidy doesn't
 				// support exporting dependencies.
-				Implicit: objFile,
+				Implicit:  objFile,
+				Implicits: cFlagsDeps,
+				OrderOnly: pathDeps,
 				Args: map[string]string{
 					"cFlags":    moduleToolingCflags,
 					"tidyFlags": flags.tidyFlags,
@@ -497,6 +499,8 @@ func TransformSourceToObj(ctx android.ModuleContext, subdir string, srcFiles and
 				Output:      sAbiDumpFile,
 				Input:       srcFile,
 				Implicit:    objFile,
+				Implicits:   cFlagsDeps,
+				OrderOnly:   pathDeps,
 				Args: map[string]string{
 					"cFlags":     moduleToolingCflags,
 					"exportDirs": flags.sAbiFlags,
