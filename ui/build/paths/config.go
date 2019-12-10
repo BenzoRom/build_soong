b8cf8f255c040a191a0560e7e324564000aa277b
Elliott Hughes <enh@google.com>
2019-12-10 10:12:18 -0800

    Switch to our hermetic bc
    Test: treehugger
    Change-Id: I12142a7f54190edd21e719bbd8f5df831ada6161
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -75,7 +75,6 @@ func GetConfig(name string) PathConfig {
 
 var Configuration = map[string]PathConfig{
 	"bash":     Allowed,
-	"bc":       Allowed,
 	"bzip2":    Allowed,
 	"ccache":   Allowed,
 	"date":     Allowed,
