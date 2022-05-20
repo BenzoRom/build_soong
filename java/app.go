c944751e28cf06904c3e56ef5dc04b6da5141e3e
Jared Duke <jdduke@google.com>
2022-05-20 12:02:13 -0700

    Use D8 by default for android_test
    android_test defaults to using R8, but with shrinking, optimization and
    obfuscation disabled, eliminating most of the benefits of R8. Instead,
    use D8 by default, improving build performance and avoiding any other
    issues that may arise in test-specific code related to whole-program R8
    execution. An initial audit shows that android_test targets that *do*
    enable shrinking or optimization also explicitly opt in to R8.
    
    A follow-up CL will do the same for android_test_helper_app, but that
    requires some additional auditing of downstream targets.
    
    Bug: 192032291
    Test: m + presubmit
    Change-Id: I5b14a0986dde210f241a77c3a93daacf9e53d667

--- java/app.go
+++ java/app.go
@@ -1031,7 +1031,7 @@ func (a *AndroidTest) OverridablePropertiesDepsMutator(ctx android.BottomUpMutat
 func AndroidTestFactory() android.Module {
 	module := &AndroidTest{}
 
-	module.Module.dexProperties.Optimize.EnabledByDefault = true
+	module.Module.dexProperties.Optimize.EnabledByDefault = false
 
 	module.Module.properties.Instrument = true
 	module.Module.properties.Installable = proptools.BoolPtr(true)
@@ -1081,6 +1081,7 @@ func (a *AndroidTestHelperApp) InstallInTestcases() bool {
 func AndroidTestHelperAppFactory() android.Module {
 	module := &AndroidTestHelperApp{}
 
+	// TODO(b/192032291): Disable by default after auditing downstream usage.
 	module.Module.dexProperties.Optimize.EnabledByDefault = true
 
 	module.Module.properties.Installable = proptools.BoolPtr(true)
